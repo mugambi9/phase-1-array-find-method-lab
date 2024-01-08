9ab5b37ca23cea9389cf21447168cbae503ba29e
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-01-08 12:16:42 +0300

    completed assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+
+function superbowlWin(records) {
+    
+    const winningRecord = records.find(record => record.result === "W");
+  
+    
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
